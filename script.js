61ce4770bd9c455ca87e4294a6235396c87502b5
asxelot <asxelot@gmail.com>
2014-07-24 21:14:13 +0300

    Finish pure js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
+var q = document.querySelector.bind(document);
 var Field = {
-    $: $('#field'),
+    q: q('#field'),
     draw: function() {
         this.h = 40;
         this.w = this.h;
 
-        this.$.html('<table>' + Array(this.h + 1).join('<tr>' +
-            Array(this.w + 1).join('<td></td>') + '</tr>') + '</table>');
+        this.q.innerHTML = '<table>' + Array(this.h + 1).join('<tr>' +
+            Array(this.w + 1).join('<td></td>') + '</tr>') + '</table>';
     },
     getCell: function(x, y) {
-        return this.$.find('tr').eq(y).find('td').eq(x);
+        return this.q.querySelector('tbody').children[y].children[x];
     },
     createApple: function() {
         var x, y;
         do {
             x = ~~(Math.random() * this.w);
             y = ~~(Math.random() * this.h);
-        } while ( this.getCell(x, y).hasClass('snake') );
-        this.getCell(x, y).addClass('apple');
+        } while ( this.getCell(x, y).className == 'snake' );
+        this.getCell(x, y).className = 'apple';
     }
 };
 
@@ -29,8 +30,10 @@ var Snake = {
         this.isTurned = false;
         this.turnQueue = null;
 
-        for (var x = 4; x--;) this.body.push(Field.getCell(x, 0)[0]);
-        $(this.body).addClass('snake');
+        for (var x = 4; x--;) this.body.push( Field.getCell(x, 0) );
+        this.body.forEach(function(cell) {
+            cell.className = 'snake';
+        });
     },
     turn: function(dir) {
         if ( Math.abs(this.dir - dir) == 2 ) return;    // ignore if press back
@@ -39,25 +42,26 @@ var Snake = {
         this.isTurned = true;
     },
     move: function() {
-        this.dir % 2 ? this.head.y += this.dir - 2 : this.head.x += this.dir - 1;
+        var headCell;
 
-        var $headCell = Field.getCell(this.head.x, this.head.y);
+        this.dir % 2 ? this.head.y += this.dir - 2 : this.head.x += this.dir - 1;
 
         if ( this.head.x < 0 || this.head.x >= Field.w ||
              this.head.y < 0 || this.head.y >= Field.h ||
-             $headCell.hasClass('snake') ) {
+             (headCell=Field.getCell(this.head.x, this.head.y)).className=='snake' ) {
             Game.over();
             return;
         }
-        this.body.unshift( $headCell.addClass('snake')[0] );
 
-        if ( $headCell.hasClass('apple') ) {
-            $headCell.removeClass('apple');
-            Game.$score.text( ++Game.score );
+        this.body.unshift( headCell );
+
+        if ( headCell.className == 'apple' ) {
+            Game.qScore.textContent = ++Game.score ;
             Field.createApple();
         } else {
-            $( this.body.pop() ).removeClass('snake');
+            ( this.body.pop() ).className = '';
         }
+        headCell.className = 'snake';
         this.isTurned = false;
     }
 };
@@ -66,31 +70,34 @@ var Game = {
     score: 0,
     highscore: 0,
     isPause: false,
-    $score: $('#score'),
-    $highscore: $('#highscore'),
-    $restart: $('#restart').on('click', 'button', function() {
-        Game.restart();
-    }),
+    isOver: false,
+    qScore: q('#score'),
+    qHighscore: q('#highscore'),
+    qRestart: q('#restart'),
     start: function() {
+        this.qRestart.onclick = function() {
+            Game.restart();
+        };
         Field.draw();
         Snake.init();
         Field.createApple();
         this.getHighscore();
         this.loop();
 
-        $(document).on('keydown', function(e) {
+        document.onkeydown = function(e) {
             var key = e.keyCode - 37;
             if ( key >= 0 && key < 4 && !Game.isPause ) Snake.turn(key);
             if ( e.keyCode == 32 ) Game.pause();
-        });
+        };
     },
     restart: function() {
-        Field.$.find('td').removeClass();
-        this.$restart.hide();
-        this.$score.text( this.score = 0 );
+        Field.draw();
+        this.qRestart.style.display = 'none';
+        this.qScore.textContent = this.score = 0 ;
         Snake.init();
         Field.createApple();
         this.loop();
+        this.isOver = false;
     },
     getHighscore: function() {
         var cookie = document.cookie;
@@ -99,13 +106,13 @@ var Game = {
         } else {
             this.setHighscore(0);
         }
-        this.$highscore.text(this.highscore);
+        this.qHighscore.textContent = this.highscore;
     },
     setHighscore: function(score) {
         var date = new Date;
         date.setDate( date.getDate() + 365 );
         document.cookie = 'highscore=' + score + '; expires=' + date.toUTCString();
-        this.$highscore.text(score);
+        this.qHighscore.textContent = score;
     },
     loop: function() {
         this.interval = setInterval(function() {
@@ -117,6 +124,7 @@ var Game = {
         }, 60);
     },
     pause: function() {
+        if ( this.isOver ) return;
         if ( !this.isPause ) {
             clearInterval(this.interval);
             this.isPause = true;
@@ -127,8 +135,10 @@ var Game = {
     },
     over: function() {
         clearInterval(this.interval);
-        this.$restart.show().find('button').focus();
+        this.qRestart.style.display = 'inline';
+        this.qRestart.querySelector('button').focus();
         if ( this.score > this.highscore ) this.setHighscore(this.score);
+        this.isOver = true;
     }
 };
 
